2553bed6cc9b74ad605f49d390d65e2c95ab655a
Deni0012 <deni.dg00@icloud.com>
2025-03-17 15:57:39 +0100

    feat: added day-2

---
+++ day-2/README.md
@@ -0,0 +1,27 @@
+#  Photo Blog
+
+### Richieste giorno 1: 
+
+- ### Milestone 1
+  - Sfruttando gli screen e gli asset in allegato riproduciamo la grafica proposta in maniera statica: utilizzando soltanto HTML e CSS e riproducendo una singola fotografia
+
+- ### Milestone 2
+
+  - Testiamo una chiamata a questo endpoint: https://lanciweb.github.io/demo/api/pictures/
+  -Studiamo bene la risposta e i dati che ci fornisce iniziando a pensare a come poterli sfruttare.
+
+
+- ### Milestone 3
+
+  - Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API, sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
+
+
+- ### Bonus 
+
+  -rendi la pagina responsive, in modo che su mobile e tablet le foto si dispongano man mano una sotto l’altra ed il titolo abbia una dimensione adeguata
+
+
+### Note 
+
+- Utilizzo di axios per effettuare una chiamata AJAX all'API 
+- Scelto di NON utilizzare bootstrap per il tipo di approccio (desktop first)
---
+++ day-2/js/main.js
@@ -0,0 +1,35 @@
+const urlApi = "https://lanciweb.github.io/demo/api/pictures/";
+const parentElement = document.getElementById("card-container");
+
+// Chiamata all'endpoint urlAPI alla risposta invoca la funzione renderHTML
+axios.get(urlApi).then(response => renderHTML(parentElement, response.data)).catch(error => console.error(error));
+
+/*
+Funzione che accetta due parametri:
+- un elemento "padre" che è il contenitore in cui appendere i nuovi elementi.
+- un array su cui itineremo per generare un nuovo elemento con createHTML per ogni elemento dell'array.
+*/
+function renderHTML(parent, childArray) {
+    parent.innerHTML = "";
+    for (let i = 0; i < childArray.length; i++) {
+        let element = childArray[i];
+        let childElement = createHTML(element);
+        parent.appendChild(childElement);
+    }
+}
+
+/*
+Funzione che accetta un parametro dalla quale utilizzeremo le proprietà del oggetto per creare un nuove elemento HTML
+*/
+function createHTML(member) {
+    let element = document.createElement("div");
+    element.classList.add("card");
+
+    element.innerHTML = ` 
+                <img src="${member.url}" alt="">
+                <h4>${member.title}.</h4>
+                <p>${member.date}.</p>
+                <img class="pin" src="img/pin.svg" alt="">`;
+
+    return element
+}
